3d87d3ec543074416ba139aaee5b7710e9c597c2
cerberus <cerberus@oriax.eu>
2022-10-30 22:04:46 +0100

    Change Main class

--- SpaceAge/src/Main.java
+++ SpaceAge/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //initialize scanner
+        Scanner scanner = new Scanner(System.in);
+        double ageInSeconds;
+
+        try {
+            System.out.print("Give me your age in seconds -> ");
+            // read age in seconds
+            ageInSeconds = scanner.nextDouble();
+        } catch (InputMismatchException e){
+            System.out.println("ERROR -> " + e);
+            ageInSeconds = 0;
+        }
+
+        // initialize SpaceAge class and read age in seconds
+        SpaceAge age = new SpaceAge(ageInSeconds);
+        System.out.println("Age on Jupiter " + age.onJupiter());
+
+        // close scannner
+        scanner.close();
+
     }
 }
